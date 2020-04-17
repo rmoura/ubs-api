876b97f39c09597901ca59d10a0dca5e4acdddc3
Rodney Moura <rodney.rms@gmail.com>
2020-04-17 10:45:05 -0300

    Specs to services Haversine and UbsFile

---
+++ spec/services/haversine_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Haversine do
+  describe '#distance' do
+    it 'is expected to calculate correctly' do
+      expect(
+        Haversine.distance(
+          -23.604936,        -46.692999,
+          -23.6099946498864, -46.7057347297655
+        )
+      ).to eq(1.4143020957176993)
+    end
+  end
+end
---
+++ spec/services/ubs_file_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe UbsFile do
+  before do
+    allow(Net::HTTP).to receive(:get).and_return(
+      File.read(File.join(Rails.root, 'spec/support/sample_file.csv'))
+    )
+  end
+
+  describe '#download' do
+    it { expect(UbsFile.download).to be_a_kind_of(UbsFile)}
+    it { expect(UbsFile.download).to have_attributes(path: start_with('/tmp/')) }
+  end
+
+  describe '#transform!' do
+    subject { UbsFile.download.transform! }
+
+    it { expect(subject).to be_a_kind_of(CSV::Table) }
+    it { expect(subject.first).to be_a_kind_of(CSV::Row) }
+
+    it 'is expected to convert header and values' do
+      expect(subject.first[:lat]).to a_value
+    end
+  end
+end
